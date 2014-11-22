8d5f677fa007c8f5e130c99ea4ee7cec1b496db0
Joel <joel_1st@hotmail.com>
2014-11-23 03:24:56 +1000

    Changed to port 80 for production

--- bin/maintenance.js
+++ bin/maintenance.js
@@ -73,7 +73,7 @@ app.use(function(err, req, res, next) {
 
 
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
