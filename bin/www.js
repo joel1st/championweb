d24ae378332eed8ace1792173715015ff11333e6
Joel <joel_1st@hotmail.com>
2014-11-25 00:46:59 +1000

    changed db file location, removed maintenance file and changed update so that aggregation can be selected every couple of weeks or so instead of every update.

--- bin/www.js
+++ bin/www.js
@@ -2,7 +2,7 @@
 "use strict";
 var debug = require('debug')('my-application');
 var app = require('../app');
-var db = require('./db');
+var db = require('../db');
 
 app.set('port', process.env.PORT || 80);
 
