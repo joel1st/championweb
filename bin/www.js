c70c81d09f5efc2bf960e094e67e3cfe2eb9b395
Joel <joel_1st@hotmail.com>
2015-01-26 02:50:52 +1000

    working on runes/masteries/skillorder

--- bin/www.js
+++ bin/www.js
@@ -4,7 +4,7 @@ var debug = require('debug')('my-application');
 var app = require('../app');
 var db = require('../db');
 
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 8000);
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
