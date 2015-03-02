65adeb4ffd2589c692e27f17f54d8fd9c453b48e
Joel <joel_1st@hotmail.com>
2015-03-02 16:02:26 +1000

    data update

--- models/data.js
+++ models/data.js
@@ -786,7 +786,7 @@
 
    var core = {
       updating : (process.env.NODE_ENV === 'updating'),
-      gamesAnalyzed:"1,424,170",
+      gamesAnalyzed:"2,486,920",
       patch:"5.4",
       ddPatch:"5.4.1",
       patchHistory: ["4.21","5.1","5.2","5.3","5.4"],
