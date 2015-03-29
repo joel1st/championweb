39011b88ee74d435d9bc7d19a54dfe159c9b798a
Joel <joel_1st@hotmail.com>
2015-03-29 21:28:50 +1000

    updated data

--- models/data.js
+++ models/data.js
@@ -792,10 +792,10 @@
 
    var core = {
       updating : (process.env.NODE_ENV === 'updating'),
-      gamesAnalyzed:"3,999,720",
-      patch:"5.5",
-      ddPatch:"5.5.2",
-      patchHistory: ["5.1","5.2","5.3","5.4","5.5"],
+      gamesAnalyzed:"1,071,120",
+      patch:"5.6",
+      ddPatch:"5.6.1",
+      patchHistory: ["5.2","5.3","5.4","5.5","5.6"],
       masteryOrder: ['Offense','Defense','Utility']
    };
 
