228b5efb16d87b77daa8838efa6e97156ab129da
Joel <joel_1st@hotmail.com>
2015-04-29 22:41:36 +1000

    updated data

--- update_data.sh
+++ update_data.sh
@@ -4,4 +4,4 @@ mongodump --db leaguetimes --collection championroles --out ./db
 mongodump --db leaguetimes --collection roles --out ./db
 mongodump --db leaguetimes --collection stattables --out ./db
 mongodump --db leaguetimes --collection summaries --out ./db
-git add -A && git commit -m "updated data" && git push
+git add -A && git commit -m "updated data" && git push origin master
