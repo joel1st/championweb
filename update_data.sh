77875551ef87b4537a4348647093c0ddb76f5a78
Joel <joel_1st@hotmail.com>
2015-05-04 23:25:59 +1000

    fixed issue with play percent/ban percent being artificially higher because I forgot to take into account overall games played rather than games where a champion was matched into a particular role

--- update_data.sh
+++ update_data.sh
@@ -1,7 +1,12 @@
+default="updated data"
+gitmsg=${1:-$default}
+echo $gitmsg
+
 mongodump --db leaguetimes --collection championdatas --out ./db
 mongodump --db leaguetimes --collection championmatchups --out ./db
 mongodump --db leaguetimes --collection championroles --out ./db
 mongodump --db leaguetimes --collection roles --out ./db
 mongodump --db leaguetimes --collection stattables --out ./db
 mongodump --db leaguetimes --collection summaries --out ./db
-git add -A && git commit -m "updated data" && git push origin master
+
+git add -A && git commit -m "$gitmsg" 
