eb2de2714800b95a93fdb7337ff8296bff3970d3
Joel <joel_1st@hotmail.com>
2015-07-24 21:31:03 +1000

    Patch 5.14

--- headline.js
+++ headline.js
@@ -1,3 +1,3 @@
 // Message to display at the top of the site.
-// Standard message: We are currently aggregating patch 5.13 data - check back in 3 days!
-module.exports = "We are currently aggregating patch 5.14 data - check back in 1 day!";
+// Standard message: We are currently aggregating patch 5.14 data - check back in 3 days!
+module.exports = "";
