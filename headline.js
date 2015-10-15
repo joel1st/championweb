8dbc92403b09cdc85c1331adce61aaac774b7665
Joel <joel_1st@hotmail.com>
2015-10-15 18:33:57 +1000

    change to two days

--- headline.js
+++ headline.js
@@ -1,3 +1,3 @@
 // Message to display at the top of the site.
 // Standard message: We are currently aggregating patch 5.17 data - check back in 3 days!
-module.exports = "We are currently aggregating patch 5.20 data - check back in 3 days!";
+module.exports = "We are currently aggregating patch 5.20 data - check back in 2 days!";
