78cad039e26b0ea010751f61156dcd74d171a293
Developver <paulxuDev@Pauls-MacBook-Air.local>
2016-03-19 12:25:21 -0400

    Fixed Things

--- routes/statistics.js
+++ routes/statistics.js
@@ -4,8 +4,6 @@ var produceError = require('../logic/produce_error.js');
 var express = require('express');
 var router = express.Router();
 router.get('/', function(req, res, next) {
-
-<<<<<<< HEAD
     WebStatisticsPage.find({}, function(err, results) {
         if (err) {
             return next(produceError('serverMaintenance', 503));
@@ -14,16 +12,6 @@ router.get('/', function(req, res, next) {
         } else {
             res.render('statistics', {
                 data: results,
-=======
-    WebStatisticsPage.find({}, function(err, doc) {
-        if (err) {
-            return next(produceError('serverMaintenance', 503));
-        } else if (!doc) {
-            return next(produceError('serverMaintenance', 503));
-        } else {
-            res.render('statistics', {
-                data: doc,
->>>>>>> 0acee52b21d5eb5856cd35d38022084123812434
                 pageData: {
                     appName: 'statsPage',
                     name: 'stats',
