1b686e02c806645583b0201e1acea3cda2f4e50d
joel1st <joel_1st@hotmail.com>
2016-03-26 00:58:23 +1000

    Update headline.js

--- headline.js
+++ headline.js
@@ -1,3 +1,3 @@
 // Message to display at the top of the site.
 // Standard message: We are currently aggregating patch 6.6 data - check back in 3 days!
-module.exports = "We are currently aggregating patch 6.6 data - check back in 2 days!";
+module.exports = "We are currently aggregating patch 6.6 data - check back in 1 day!";
