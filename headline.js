c7d808d7288b0771bdbd9194272ef9171a837e62
Joel <joel_1st@hotmail.com>
2016-03-29 22:49:45 +1000

    updated data

--- headline.js
+++ headline.js
@@ -1,3 +1,3 @@
 // Message to display at the top of the site.
 // Standard message: We are currently aggregating patch 6.6 data - check back in 3 days!
-module.exports = "There is currently an issue with Aurelion Sol's experience data.";
+module.exports = "";
