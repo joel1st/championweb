15dbe25f57bae543b6c2d7a7cfebff3c1e8cb638
Joel <joel_1st@hotmail.com>
2016-08-14 10:02:29 +1000

    ...

--- headline.js
+++ headline.js
@@ -1,3 +1,3 @@
 // Message to display at the top of the site.
 // Standard message: We are currently aggregating patch 6.14 data - check back in 3 days!
-module.exports = "We are currently aggregating patch 6.16 data - check back in 1 day!";
+module.exports = "";
